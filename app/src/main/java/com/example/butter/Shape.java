6a33fed526a0f240eb3965fa315dcaf506b22e8e
Soopyman <soopyman@gmail.com>
2024-09-26 18:43:06 -0600

    color thing

--- app/src/main/java/com/example/butter/Shape.java
+++ app/src/main/java/com/example/butter/Shape.java
@@ -4,9 +4,16 @@ abstract public class Shape {
 
     private int x;
     private int y;
+    private String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Shape(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
     }
 }
